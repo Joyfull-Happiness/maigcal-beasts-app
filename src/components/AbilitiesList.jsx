9770ef65460453e09b02f43c645489ac898cc09e
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-07-28 09:07:48 -0700

    updated bugs

--- src/components/AbilitiesList.jsx
+++ src/components/AbilitiesList.jsx
@@ -21,21 +21,3 @@ function AbilitiesList({ creaturesData }) {
 }
 export default AbilitiesList; 
 
-
-
-function AbilitiesList({ creaturesData }) {
-   
-    return (
-
-        <div >
-
-<h4>Abilities List</h4>
-    <ol>
-        
-    </ol>
-       {creaturesData.map((creature) => (
- <CreatureCard key={creature.abilities} />)
-    
-         ) }
-                 
-        </div>
